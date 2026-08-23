f0b25167643b844f705e847d7946f2ab2b5f3db0
Christopher Jr Riley <chrisjr@cjrriley.com>
2026-08-23 06:49:35 -0400

    Add regrestation unit tests

---
+++ Tests/ATProtoKitTests/Regessions/Regressions.swift
@@ -0,0 +1,13 @@
+//
+//  Regressions.swift
+//  ATProtoKit
+//
+//  Created by Christopher Jr Riley on 2026-08-23.
+//
+
+import Foundation
+import Testing
+@testable import ATProtoKit
+
+@Suite("Regression Tests")
+public struct RegressionTests {}
